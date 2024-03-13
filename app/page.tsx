4c10e3523be729c6b6b81dbd50bd97a8b82f9051
Ndzi Nfor <ndzinfor@gmail.com>
2024-03-13 15:13:12 +0100

    install and configure daisyui

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1 className='text-center underline'>Next 13</h1>
+      <button className='btn'>click me</button>
     </main>
   )
 }
