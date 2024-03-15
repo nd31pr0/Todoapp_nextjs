8890571f81da32972d99489b4c98b11e5657f31c
Ndzi Nfor <ndzinfor@gmail.com>
2024-03-15 08:13:27 +0100

    install and setup json server

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function TodoList() {
   return (
-    <div>TodoList</div>
+    <div className="overflow-x-auto">
+        <table className="table w-full">
+            {/* head */}
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Favorite Color</th>
+            </tr>
+            </thead>
+            <tbody>
+            {/* row 1 */}
+            <tr>
+                <td>Cy Ganderton</td>
+                <td>Blue</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
   )
 }
 
